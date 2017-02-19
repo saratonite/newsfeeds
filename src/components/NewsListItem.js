40bff47880d3667661374b60ba989321061825d7
Sarath <sarath.hacks@gmail.com>
2017-02-19 21:46:02 +0530

    Added image

--- src/components/NewsListItem.js
+++ src/components/NewsListItem.js
@@ -14,8 +14,14 @@ export default class NewsListItem extends Component {
     let feedData = this.props.data;
     return(
       <div className="feed-list-item">
+        <div className="feed-image">
+          <img src={feedData.image.url} />
+        </div>
+        <div className="feed-body">
         <strong>{feedData.title}</strong>
         <p>{feedData.summary}</p>
+        </div>
+
       </div>
     )
   }
